2b6620db9d7b0eb16e29edd23e21246c42e86b65
Ana Silva <ananunesdasilva@gmail.com>
2017-12-12 14:22:03 +0000

    devise models for student and publisher

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :publishers, path: 'publishers'
-  devise_for :students, path: 'students'
+  devise_for :publishers, path: 'publishers', controllers: { sessions: "publishers/sessions" }
+  devise_for :students, path: 'students', controllers: { sessions: "students/sessions" }
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

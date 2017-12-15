25a13fedee91277895489bae42cd42fed75a6aae
Ana Silva <ananunesdasilva@gmail.com>
2017-12-15 10:00:26 +0000

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_for :students, path: 'students'
   root to: 'pages#home'
 
+
   resources :courses, only: [ :index, :new, :create, :embed]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

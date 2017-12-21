3f0cac01d730249bac6d3462e4813d32a6fe0d01
Ana Silva <ananunesdasilva@gmail.com>
2017-12-21 16:15:08 +0000

    improved seeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171219120632) do
+ActiveRecord::Schema.define(version: 20171221152226) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -69,6 +69,7 @@ ActiveRecord::Schema.define(version: 20171219120632) do
     t.inet "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
     t.index ["email"], name: "index_publishers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_publishers_on_reset_password_token", unique: true
   end
